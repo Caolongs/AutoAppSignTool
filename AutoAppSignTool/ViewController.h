af91287d0d8d26ba5358944ed45e798d351ecd82
cao longjian <caolongs@126.com>
2018-05-10 15:35:16 +0800

    sign

---
+++ AutoAppSignTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoAppSignTool
+//
+//  Created by cao longjian on 2018/5/10.
+//  Copyright © 2018年 caolongjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
